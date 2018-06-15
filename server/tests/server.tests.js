a1fe03a7cfa7e2fbec1620b195939e26b7850724
BRAJESH  VERMA <vermabrajesh12@gmail.com>
2018-06-15 17:39:02 +0530

    add test for GET /todos

--- server/tests/server.tests.js
+++ server/tests/server.tests.js
@@ -2,8 +2,17 @@ const expect=require('expect');
 const request=require('supertest');
 const {app}=require('./../server');
 const {Todo}=require('./../models/todo');
+
+const todos=[{
+  text:'first test todo',
+  text:'second test todo'
+}];
+
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
+  Todo.remove({}).then(()=>{
+  return Todo.insertMany(todos);
+}).then(()=>done());
 });
 describe('POST /todos',()=>{
   it('should create a new todo',(done)=>{
@@ -19,10 +28,10 @@ describe('POST /todos',()=>{
       if(err){
         return done(err);
       }
-      Todo.find().then((todos)=>{
+      Todo.find({text}).then((todos)=>{
         expect(todos.length).toBe(1);
 
-        expect(todos[0].text).toBe(text)
+        expect(todos[0].text).toBe(text);
 done();
 }).catch((e)=>done(e));
     });
@@ -37,12 +46,20 @@ it('should not create todo with invalid body data',(done)=>{
       return done(err);
     }
     Todo.find().then((todos)=>{
-      expect(todos.length).toBe(0);
+      expect(todos.length).toBe(1);
       done();
     }).catch((e)=>done(e));
   });
 });
-
-
-  
+});
+describe('GET /todos',(done)=>{
+it('should get all todos',(done)=>{
+  request(app)
+  .get('/todos')
+  .expect(200)
+  .expect((res)=>{
+    expect(res.body.todos.length).toBe(1);
+  })
+  .end(done);
+});
 });
