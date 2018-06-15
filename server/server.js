73a43d1aad99b816a586f9d871f1c94ced9ac552
BRAJESH  VERMA <vermabrajesh12@gmail.com>
2018-06-15 17:07:27 +0530

    test post todos route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,17 @@ res.send(doc);
 app.listen(3000,()=>{
   console.log('started on port 3000');
 });
+
+module.exports={app};
+
+
+
+
+
+
+
+
+
 // var Todo=mongoose.model('Todo',{
 //   text:{
 //     type:String,
